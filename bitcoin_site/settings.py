267412c6e992b88f23bb8cd6478b0d8c69bb9ae6
victor mbah <vmbah231@gmail.com>
2025-06-21 12:19:54 -0700

    Prepare for Railway deployment

--- bitcoin_site/settings.py
+++ bitcoin_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=p!af%1&r$cane))#=3t2twz5l!ul$!v%ujv3ohnnif=d@*m8b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
